6e91e358fa4cbe82bddef89fa79128e82dcb0157
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-09-10 23:12:12 +0300

    Update converterJson_test.cpp

--- tests/converterJson_test.cpp
+++ tests/converterJson_test.cpp
@@ -3,25 +3,22 @@
 #include <fstream>
 
 TEST(ConverterJSONTest, ConfigValidation) {
-    // Тест проверки конфигурации
     EXPECT_TRUE(ConverterJSON::fileConfigVerify());
 }
 
 TEST(ConverterJSONTest, GetResponsesLimit) {
-    // Тест получения лимита ответов
     int limit = ConverterJSON::GetResponsesLimit();
     EXPECT_GE(limit, 1);
     EXPECT_LE(limit, 100);
 }
 
 TEST(ConverterJSONTest, GetTextDocuments) {
-    // Тест получения текстов документов
     auto docs = ConverterJSON::GetTextDocuments();
     EXPECT_FALSE(docs.empty());
 }
 
 TEST(ConverterJSONTest, GetRequests) {
-    // Тест получения запросов
     auto request = ConverterJSON::GetRequests();
     EXPECT_FALSE(request.empty());
+
 }
